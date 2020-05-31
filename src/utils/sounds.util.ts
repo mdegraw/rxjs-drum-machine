07fa5c57da1e935f82acaf5c1912371ef756b5b6
mdegraw <michael.degraw@semanticbits.com>
2020-05-30 22:54:30 -0600

    Undo

--- src/utils/sounds.util.ts
+++ src/utils/sounds.util.ts
@@ -1,50 +1,50 @@
+const snare = new Audio('/sounds/snare.mp3');
+const kick = new Audio('/sounds/kick.mp3');
+const hihat = new Audio('/sounds/hihat.mp3');
+const maracas = new Audio('/sounds/maracas.mp3');
+const clave = new Audio('/sounds/clave.mp3');
+const rimshot = new Audio('/sounds/rimshot.mp3');
+const toms = new Audio('/sounds/toms.mp3');
+const clap = new Audio('/sounds/clap.mp3');
+const conga = new Audio('/sounds/conga.mp3');
+const cowbell = new Audio('/sounds/cowbell.mp3');
 
 export const playKick = () => {
-  const kick = new Audio('/sounds/kick.mp3');
   kick.play();
 }
 
 export const playHiHat = () => {
-  const hihat = new Audio('/sounds/hihat.mp3');
   hihat.play();
 };
 
 export const playSnare = () => {
-  const snare = new Audio('/sounds/snare.mp3');
   snare.play();
 };
 
 export const playMaracas = () => {
-  const maracas = new Audio('/sounds/maracas.mp3');
   maracas.play();
 };
 
 export const playRimshot = () => {
-  const rimshot = new Audio('/sounds/rimshot.mp3');
   rimshot.play();
 };
 
 export const playClave = () => {
-  const clave = new Audio('/sounds/clave.mp3');
   clave.play();
 };
 
 export const playClap = () => {
-  const clap = new Audio('/sounds/clap.mp3');
   clap.play();
 };
 
 export const playCowbell = () => {
-  const cowbell = new Audio('/sounds/cowbell.mp3');
   cowbell.play();
 };
 
 export const playToms = () => {
-  const toms = new Audio('/sounds/toms.mp3');
   toms.play();
 };
 
 export const playConga = () => {
-  const conga = new Audio('/sounds/conga.mp3');
   conga.play();
 };
