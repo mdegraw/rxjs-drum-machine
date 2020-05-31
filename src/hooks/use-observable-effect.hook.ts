ea48412d175c2bd0117ba8e426c8d9b6eb605023
mdegraw <michael.degraw@semanticbits.com>
2020-05-30 22:02:21 -0600

    Update useObservableEffect hook

--- src/hooks/use-observable-effect.hook.ts
+++ src/hooks/use-observable-effect.hook.ts
@@ -2,13 +2,14 @@ import { useEffect } from 'react';
 import { Observable } from 'rxjs';
 
 /* eslint-disable react-hooks/exhaustive-deps */
-export function useObservableEffect<T>(observableS: Observable<T>, subscriptionCb?: (...args: any[]) => void, cb?: () => void) {
+export function useObservableEffect<T>(observableS: Observable<T>, subscriptionCb?: (...args: any[]) => void, cb?: (...args: any[]) => void) {
   useEffect(() => {
     const subscription = observableS.subscribe(subscriptionCb);
-    // (clock$ as ConnectableObservable<IState>).connect();
+
     if (cb && typeof cb === 'function') {
       cb();
     }
+
     return () => subscription.unsubscribe();
   }, []);
 }
